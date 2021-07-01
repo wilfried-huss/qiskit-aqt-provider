654bcdfc6cbbb0f1e3dfb170cf4ef7570253e2a3
Wilfried Huss <wilfried.huss@aqt.eu>
2021-07-01 11:16:51 +0200

    add Ibex device, add dummy noise model for noisy simulator

--- qiskit_aqt_provider/aqt_backend.py
+++ qiskit_aqt_provider/aqt_backend.py
@@ -12,6 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+from typing import List
 import warnings
 
 import requests
@@ -19,7 +20,7 @@ import requests
 from qiskit import qobj as qobj_mod
 from qiskit.providers import BackendV1 as Backend
 from qiskit.providers import Options
-from qiskit.providers.models import BackendConfiguration
+from qiskit.providers.models import BackendConfiguration, BackendProperties
 from qiskit.exceptions import QiskitError
 from qiskit.util import deprecate_arguments
 
@@ -27,6 +28,7 @@ from . import aqt_job
 from . import qobj_to_aqt
 from . import circuit_to_aqt
 
+MICROSECONDS = "µs"
 
 class AQTSimulator(Backend):
 
@@ -101,22 +103,32 @@ class AQTSimulator(Backend):
         return job
 
 
+def fully_connected_coupling_map(n_ions: int) -> List[List[int]]:
+    coupling_map = []
+    for i in range(n_ions):
+        for j in range(i+1, n_ions):
+            coupling_map.append([i, j])
+    return coupling_map
+
+
 class AQTSimulatorNoise1(Backend):
+    _N_QUBITS: int = 11
+    _DATE: str = "2000-01-01 00:00:00Z"
+    url: str = "https://gateway.aqt.eu/marmot/sim/noise-model-1"
 
     def __init__(self, provider):
-        self.url = "https://gateway.aqt.eu/marmot/sim/noise-model-1"
         configuration = {
             'backend_name': 'aqt_qasm_simulator_noise_1',
             'backend_version': '0.0.1',
             'url': self.url,
             'simulator': True,
             'local': False,
-            'coupling_map': None,
+            'coupling_map': fully_connected_coupling_map(self._N_QUBITS),
             'description': 'AQT trapped-ion device simulator '
                            'with noise model 1',
             'basis_gates': ['rx', 'ry', 'rxx'],
             'memory': False,
-            'n_qubits': 11,
+            'n_qubits': self._N_QUBITS,
             'conditional': False,
             'max_shots': 200,
             'max_experiments': 1,
@@ -132,6 +144,57 @@ class AQTSimulatorNoise1(Backend):
         super().__init__(
             configuration=BackendConfiguration.from_dict(configuration),
             provider=provider)
+
+    def properties(self):
+        """Return backend properties"""
+        coupling_map: List[List[int]] = self.configuration().coupling_map
+        unique_qubits: List[int] = list(set().union(*coupling_map))
+
+        # TODO: provide correct values
+        properties = {
+            "backend_name": self.name(),
+            "backend_version": self.configuration().backend_version,
+            "last_update_date": self._DATE,
+            "qubits": [
+                [
+                    {"date": self._DATE, "name": "T1", "unit": MICROSECONDS, "value": 0.0},
+                    {"date": self._DATE, "name": "T2", "unit": MICROSECONDS, "value": 0.0},
+                    {
+                        "date": self._DATE,
+                        "name": "frequency",
+                        "unit": "GHz",
+                        "value": 0.0,
+                    },
+                    {
+                        "date": self._DATE,
+                        "name": "readout_error",
+                        "unit": "",
+                        "value": 0.0,
+                    },
+                    {"date": self._DATE, "name": "operational", "unit": "", "value": 1},
+                ]
+                for _ in range(len(unique_qubits))
+            ],
+            "gates": [
+                {
+                    "gate": "cx",
+                    "name": "CX" + str(pair[0]) + "_" + str(pair[1]),
+                    "parameters": [
+                        {
+                            "date": self._DATE,
+                            "name": "gate_error",
+                            "unit": "",
+                            "value": 0.0,
+                        }
+                    ],
+                    "qubits": [pair[0], pair[1]],
+                }
+                for pair in coupling_map
+            ],
+            "general": [],
+        }
+
+        return BackendProperties.from_dict(properties)
 
     @classmethod
     def _default_options(cls):
@@ -245,4 +308,73 @@ class AQTDevice(Backend):
         if 'id' not in response:
             raise Exception
         job = aqt_job.AQTJob(self, response['id'], qobj=circuit)
+        return job
+
+
+class AQTDeviceIbex(Backend):
+
+    def __init__(self, provider):
+        self.url = 'https://gateway.aqt.eu/marmot/Ibex'
+        configuration = {
+            'backend_name': 'aqt_ibex',
+            'backend_version': '0.0.1',
+            'url': self.url,
+            'simulator': False,
+            'local': False,
+            'coupling_map': None,
+            'description': 'AQT trapped-ion device',
+            'basis_gates': ['rx', 'ry', 'rxx', 'ms'],
+            'memory': False,
+            'n_qubits': 10,
+            'conditional': False,
+            'max_shots': 200,
+            'max_experiments': 1,
+            'open_pulse': False,
+            'gates': [
+                {
+                    'name': 'TODO',
+                    'parameters': [],
+                    'qasm_def': 'TODO'
+                }
+            ]
+        }
+        super().__init__(
+            configuration=BackendConfiguration.from_dict(configuration),
+            provider=provider)
+
+    @classmethod
+    def _default_options(cls):
+        return Options(shots=100)
+
+    def run(self, qobj, **kwargs):
+        if isinstance(qobj, qobj_mod.QasmQobj):
+            warnings.warn("Passing in a QASMQobj object to run() is "
+                          "deprecated and will be removed in a future "
+                          "release", DeprecationWarning)
+            if qobj.config.shots > self.configuration().max_shots:
+                raise ValueError('Number of shots is larger than maximum '
+                                 'number of shots')
+            aqt_json = qobj_to_aqt.qobj_to_aqt(
+                qobj, self._provider.access_token)[0]
+        elif isinstance(qobj, qobj_mod.PulseQobj):
+            raise QiskitError("Pulse jobs are not accepted")
+        else:
+            for kwarg in kwargs:
+                if kwarg != 'shots':
+                    warnings.warn(
+                        "Option %s is not used by this backend" % kwarg,
+                        UserWarning, stacklevel=2)
+            out_shots = kwargs.get('shots', self.options.shots)
+            aqt_json = circuit_to_aqt.circuit_to_aqt(
+                qobj, self._provider.access_token, shots=out_shots)[0]
+        header = {
+            "Ocp-Apim-Subscription-Key": self._provider.access_token,
+            "SDK": "qiskit"
+        }
+        res = requests.put(self.url, data=aqt_json, headers=header)
+        res.raise_for_status()
+        response = res.json()
+        if 'id' not in response:
+            raise Exception
+        job = aqt_job.AQTJob(self, response['id'], qobj=qobj)
         return job
--- qiskit_aqt_provider/aqt_provider.py
+++ qiskit_aqt_provider/aqt_provider.py
@@ -13,9 +13,11 @@
 # that they have been altered from the originals.
 
 
-from qiskit.providers.providerutils import filter_backends
 from qiskit.providers.exceptions import QiskitBackendNotFoundError
-from .aqt_backend import AQTSimulator, AQTSimulatorNoise1, AQTDevice
+from qiskit.providers.providerutils import filter_backends
+
+from .aqt_backend import (AQTDevice, AQTDeviceIbex, AQTSimulator,
+                          AQTSimulatorNoise1)
 
 
 class AQTProvider():
@@ -48,7 +50,8 @@ class AQTProvider():
         # Populate the list of AQT backends
         self.backends = BackendService([AQTSimulator(provider=self),
                                         AQTSimulatorNoise1(provider=self),
-                                        AQTDevice(provider=self)])
+                                        AQTDevice(provider=self),
+                                        AQTDeviceIbex(provider=self)])
 
     def __str__(self):
         return "<AQTProvider(name={})>".format(self.name)
